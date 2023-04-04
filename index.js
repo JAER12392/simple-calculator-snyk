1dad64f7762b9413a595d558b827a11388a117c8
Zeus <jesus.esquivel@snyk.io>
2023-04-04 11:32:50 -0600

    Adding a new port for this app 5800 and added my name to the calculator

---
+++ index.js
@@ -0,0 +1,34 @@
+//Hapi framework
+const Hapi = require('hapi');
+
+//Address and port
+const host = 'localhost';
+const port = 5800;; 
+
+//Create server
+const server = Hapi.Server({
+    host: host,
+    port: port
+});
+
+//Start server
+const init = async () => {
+    await server.start();
+    console.log("Server up! Port: " + port);
+}
+//About
+    server.route({
+        method: 'GET',
+        path: '/calculator/about',
+        handler: function (request, h) {
+    
+            var data = {
+                message: 'API calculator @zeus-snyk'
+            };
+    
+            return data;
+        }
+    });
+//Start App
+init();
+
